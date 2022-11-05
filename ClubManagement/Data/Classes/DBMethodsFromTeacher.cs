a850df3999c682cf3df28890aa1e03ec77d96f1d
diorblack <45602451+claudy1337@users.noreply.github.com>
2022-11-05 16:22:00 +0300

    fix teacer

---
+++ ClubManagement/Data/Classes/DBMethodsFromTeacher.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using ClubManagement.Data.Model;
+
+namespace ClubManagement.Data.Classes
+{
+    internal class DBMethodsFromTeacher
+    {
+        public static Teacher CurrentTeacher;
+        public static ObservableCollection<Teacher> GetTeachers()
+        {
+            return new ObservableCollection<Teacher>(DBConnection.connect.Teacher);
+        }
+        public static Teacher GetTeacher(string login, string password)
+        {
+            return GetTeachers().FirstOrDefault(t => t.User.Authorization.Login == login && t.User.Authorization.Password == password);
+        }
+        public static void AddTeacher(int typeTeacher, bool isActive)
+        {
+            var getAdmin = DBMethodsFromUser.GetAdminRole(DBMethodsFromUser.CurrentUser.Authorization.Login);
+            if (DBMethodsFromUser.CurrentUser != null && getAdmin == false)
+            {
+                Teacher teacher = new Teacher
+                {
+                    idUser = DBMethodsFromUser.CurrentUser.ID,
+                    idTypeTeacher = typeTeacher,
+                    isActive = isActive
+                };
+                CurrentTeacher = teacher;
+                MessageBox.Show("учитель добавлен");
+                DBConnection.connect.Teacher.Add(teacher);
+                DBConnection.connect.SaveChanges();
+            }
+            else
+            {
+                MessageBox.Show("данные уже существуют");
+                return;
+            }
+        }
+        public static void EditTeacher(Teacher Oldteacher, int typeTeacher, bool isActive)
+        {
+            var getTeacher = GetTeacher(Oldteacher.User.Authorization.Login, Oldteacher.User.Authorization.Password);
+            if (getTeacher != null)
+            {
+                getTeacher.idTypeTeacher = typeTeacher;
+                getTeacher.isActive = isActive;
+                DBConnection.connect.SaveChanges();
+            }
+
+        }
+    }
+}
--- ClubManagement/Pages/AdminControl/ControlTeacherPage.xaml.cs
+++ ClubManagement/Pages/AdminControl/ControlTeacherPage.xaml.cs
@@ -15,6 +15,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using ClubManagement.Pages;
 using ClubManagement.Data.Classes;
+using Microsoft.Win32;
+using System.IO;
 
 namespace ClubManagement.Pages.AdminControl
 {
@@ -31,6 +33,91 @@ namespace ClubManagement.Pages.AdminControl
         {
             CurretTeacher = currnetTeacher;
             InitializeComponent();
+            if (CurretTeacher.idUser == null || CurretTeacher.idTypeTeacher == null)
+            {
+                isNullData();
+            }
+            else
+            {
+                BindingData();
+                txtLogin.IsReadOnly = true;
+            }
+            cbTypeTeacher.ItemsSource = DBConnection.connect.TypeTeacher.ToList();
+        }
+        private void BindingData()
+        {
+            this.DataContext = CurretTeacher;
+            txtEditOrAdd.Text = "Редактировать Учителя";
+            btnEditOrAdd.Content = "Edit";
+            cbTypeTeacher.SelectedIndex = CurretTeacher.TypeTeacher.id;
+            if (CurretTeacher.isActive == true)
+                CBIsActice.IsChecked = true;
+        }
+        private void isNullData()
+        {
+            txtEditOrAdd.Text = "Добавить Учителя";
+            btnEditOrAdd.Content = "Добавить";
+            string imagePath = "/Resources/default_user.png";
+            imgTeacher.Source = new BitmapImage(new Uri(imagePath, UriKind.RelativeOrAbsolute));
+        }
+        private void cbTypeTeacher_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void imgTeacher_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            imgClick = true;
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.FilterIndex = 1;
+            if (ofd.ShowDialog() == true)
+            {
+                BitmapImage bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.UriSource = new Uri(ofd.FileName);
+                bitmapImage.EndInit();
+                imgTeacher.Source = bitmapImage;
+                image = File.ReadAllBytes(ofd.FileName);
+            }
+        }
+
+        private void btnEditOrAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtName.Text) || string.IsNullOrWhiteSpace(txtPassword.Text) || string.IsNullOrWhiteSpace(txtPatronymic.Text)
+                || string.IsNullOrWhiteSpace(txtLogin.Text))
+            {
+                MessageBox.Show("заполните все данные");
+                return;
+            }
+            else
+            {
+                var selectType = cbTypeTeacher.SelectedIndex;
+                if (CurretTeacher.idUser == null || CurretTeacher.idTypeTeacher == null)
+                {
+                    DBMethodsFromUser.AddAuth(txtLogin.Text, txtPassword.Text);
+                    DBMethodsFromUser.AddUser(image, txtName.Text, txtSurname.Text, txtPatronymic.Text);
+                    DBMethodsFromTeacher.AddTeacher(selectType, isActive);
+                    Refresh();
+                }
+                else
+                {
+                    DBMethodsFromUser.EditImageUser(CurretTeacher.User);
+                    DBMethodsFromUser.EditAccount(CurretTeacher.User, txtName.Text, txtSurname.Text, txtPatronymic.Text);
+                    DBMethodsFromTeacher.EditTeacher(CurretTeacher, selectType, isActive);
+                    Refresh();
+
+                }
+            }
+        }
+        private void Refresh()
+        {
+            NavigationService.Navigate(new TeacherPage());
+        }
+
+        private void CBIsActice_Checked(object sender, RoutedEventArgs e)
+        {
+            CBIsActice.IsChecked = true;
+            isActive = true;
         }
     }
 }
